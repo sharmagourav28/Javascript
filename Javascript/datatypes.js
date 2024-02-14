9d45bf91b9c35e83c4b55fd1f36810ac1d916f56
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-14 16:42:30 +0000

    new file:   Javascript/datatypes.js

---
+++ Javascript/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as new version
+
+// alert("Hello world")  we are using node.js ,not browser
+
+// code readability is high must be
+
+
+//// --->> premitive datatypes
+let name = "Gourav";
+console.log(typeof(name));  //string "" &&  ''
+
+let age = 12;
+console.log(typeof(age));  // number  bigint
+
+let islog = false;
+console.log(typeof(islog));  // bool true and false
+
+//NULL standalone value    repsrent as empty value
+
+// undefined  value doesn't defines
+
+// symbol (react) unique
+
+
+
+//object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
