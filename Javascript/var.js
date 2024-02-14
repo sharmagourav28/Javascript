cde2dadc641471c846200438e0828817b3d72acf
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-14 16:27:52 +0000

    new file:   Javascript/var.js

---
+++ Javascript/var.js
@@ -0,0 +1,20 @@
+const accountId = 101; // not change futher
+
+let accountEmail = "shr@gmail.com";
+var accountPswd = "1223232";
+accountCity ="Indore";
+
+let accountstate; // take as undefined
+
+//log
+console.log(accountEmail);
+console.log(accountId);
+console.log(accountPswd);
+
+//table
+console.table([accountEmail,accountId,accountPswd,accountCity,accountstate]);
+// its provide a table structuree attribute index and values
+
+/*
+const and let must be used beacuse of issue in block and functional scope
+*/
