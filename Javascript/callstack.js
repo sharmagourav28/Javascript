3746ed490bbf79b59bbeb1c70c121a8bc57c3b90
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-19 15:12:10 +0000

    Call Stack

---
+++ Javascript/callstack.js
@@ -0,0 +1,20 @@
+/*
+    global exceution contest    
+    funciton execution contest
+    eval execuation contest
+
+    Global execution phase->
+              {} -> Memory creation phase   [varaible k liya space alocate hote hae]
+                 -> Execution phase    [operation's are performed on this phase]
+*/
+
+
+let val1 = 10;
+let val2 = 5
+
+function addnum(num1,num2){
+    let t = num1+num2;
+    return t;
+}
+let r = addnum(val1,val2);
+let res = addnum(10,2);
