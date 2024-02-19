a61b9355c69dcafb3f3b3e700cdd6739d3946396
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-19 06:28:51 +0000

    Function Part 2

---
+++ Javascript/function2.js
@@ -0,0 +1,28 @@
+function calculateprice(val1,va2,...num1){
+    return num1;
+}
+const arr = calculateprice(2,2,33,44,4); 
+console.log(arr)
+
+
+const user = {
+    urname :"Gourav",
+    price :199
+}
+function handleobj(anyobj){
+    console.log(`username is ${anyobj.urname} and price is ${anyobj.price}`)
+}
+handleobj(user)
+
+handleobj({ // objects
+    usernmae : "sam",
+    price:"99"
+})
+
+
+const myarr = [200,2002,200,200];
+
+function  ret(getarr){
+    return getarr[1];
+}
+console.log(ret(myarr))
