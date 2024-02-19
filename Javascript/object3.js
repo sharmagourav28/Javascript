f121b25b3dbd32705091d192c4a60bd67cc42840
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-19 06:00:34 +0000

    Object DOne

---
+++ Javascript/object3.js
@@ -0,0 +1,36 @@
+//destructuing objects
+
+
+const course ={
+    name:"jshindi",
+    price:"999",
+    inst:"Harsh",
+}
+
+const {name} = course
+const {pirce:p} = course
+console.log(name);
+console.log(p)
+
+
+// const navabr = ({Company})=>{
+
+// }
+// navabr(Company ="Gourav")
+
+
+
+////////////-------------API-------------/////
+
+// {
+//     "name" :"shyam",
+//     "coursename":"Javascript",
+//     "price":"free",
+// }
+
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
