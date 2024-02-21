d4e103eed691780e2ee018f2862cf5c9ae8e2e39
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-21 04:48:08 +0000

    modified:   Javascript/callstack.js 	new file:   Javascript/controlflow.js

---
+++ Javascript/controlflow.js
@@ -0,0 +1,45 @@
+// // if statements
+
+
+// const isuser = true;
+
+// if(isuser){
+
+// }
+// // comparison operatior <,>,<=,>=,==,=== to check datatypes ,!=,!==
+// if(2==2){
+
+// }
+// else{
+
+// }
+
+// const sc = 200;
+// console.log(sc)
+// if(sc>100){
+//     const pow = "fly";
+//     console.log(pow);
+// }
+
+const bal = 1000;
+if(bal>200) console.log("test");
+
+
+// if(){
+
+// }
+// else if(){
+                    // IF ELSE LADDER
+// }
+// else{
+
+// }
+  
+
+// (  &&   ){
+
+// }
+
+// if( || ){
+
+// }
