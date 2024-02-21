0800c433adca80bd2811f4399f312974b7a11a6e
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-21 04:51:53 +0000

    new file:   Javascript/switch.js

---
+++ Javascript/switch.js
@@ -0,0 +1,24 @@
+//switch 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const mon = 6
+switch(mon){
+    case 1:
+            console.log("jan");
+            break;
+    case 2:
+            console.log("Feb");
+            break;
+    case 3:
+            console.log("March");
+            break;
+    default:
+        console.log("not found");
+}
