ddec0dd9967e8b2eb3a86e7522f49568815a6488
Gourav Sharma <136950130+sharmagourav28@users.noreply.github.com>
2024-02-22 07:01:02 +0000

    DOM

---
+++ Javascript/otherloop.js
@@ -0,0 +1,21 @@
+//for of
+// ["","",""];
+// [{},{}]
+
+const arr = [1,2,3,4,5,6];
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+const str = ["hwlo","frr","ee"];
+for (const i of str) {
+    console.log(i)
+    // console.log(`each char ${i}`);
+}
+
+//maps
+
+const maps = new Map();
+maps.set('hee',"ff");
+
+console.log(maps)
